34e34544cd9aa8ba81737104ffcbc71929c4a81a
agostter <115723223+agostter@users.noreply.github.com>
2022-11-30 11:35:27 -0300

    Add files via upload

---
+++ CLASE01/js/index.js
@@ -0,0 +1,22 @@
+
+console.log ("Buenos días. Soy el código JS del extremo del html")
+
+console.log ('Buenos días. Soy el código JS del extremo del html')
+
+//Variables
+//Palabras reserverdas
+var nombre;
+
+nombre="Agostina"
+
+console.log ("Mi nombre es: " + nombre);
+
+let apellido ="Termine"
+
+const provincia = "Salta";
+
+nombre = "Pepe";
+apellido = "Sanchez";
+
+console.log ("Mi nombres es ${nombre} y mi apellido es ${apellido} y vivo en ${provincia}")
+
